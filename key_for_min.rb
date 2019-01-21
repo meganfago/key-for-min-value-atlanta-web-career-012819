5a5da16fb31c538c53f9a99f5567a8ea3ff92b67
Megan Fago <meganlaynefago@gmail.com>
2019-01-21 01:07:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  if name_hash = {}
-    binding.pry
+  if name_hash == {}
     return nil
   else
       min_value = name_hash.first[1]
